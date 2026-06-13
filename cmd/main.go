107d30c733bc2f0329867b910b323e072b4e7fed
umesshk <umeshkumar01439@gmail.com>
2026-06-13 17:09:59 +0530

    xml files

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 
 	page_url := flag.String("link", "google.com", "Provide the url of page you want to build site map")
-	max_depth := flag.Int("depth", 1, "provide max depth of links to follow from a  page ")
+	max_depth := flag.Int("depth", 3, "provide max depth of links to follow from a  page ")
 
 	flag.Parse()
 
