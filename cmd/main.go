ef848a3d81d6f534fea13c43865d3f634cde94f0
umesshk <umeshkumar01439@gmail.com>
2026-06-16 15:31:48 +0530

    max depth

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 
 	page_url := flag.String("link", "", "Provide the url of page you want to build site map")
-	max_depth := flag.Int("depth", 3, "provide max depth of links to follow from a  page ")
+	max_depth := flag.Int("depth", 3, "provide max depth of links to follow from a  page (max 10)")
 
 	flag.Parse()
 
@@ -19,6 +19,11 @@ func main() {
 		fmt.Println("Please Provide a URL")
 		os.Exit(-1)
 
+	}
+
+	if *max_depth >= 10 {
+		fmt.Println("Max depth limited to 10")
+		os.Exit(-1)
 	}
 
 	links := builder.BuildMap(*page_url, *max_depth)
